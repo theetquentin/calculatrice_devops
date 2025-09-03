d0d2a160dbf8e35205fb8ae7853ee15d5f6ed22a
theetquentin <quentin.theet@gmail.com>
2025-09-03 11:49:23 +0200

    ajout des tests en cas d'erreur sur la route post /api/addition

--- server.test.js
+++ server.test.js
@@ -12,13 +12,14 @@ describe("Test sur l'api", () => {
 
     })
 
-
 });
 
 describe("POST /api/addition (mock req/res)", () => {
 
         test("5 + 3 = 8", () => {
         const req = { body: { nombre1: 5, nombre2: 3 } };
+        // jest.fn().mockReturnThis() permet de chaîner comme en express
+        // pour simuler res.status(200).json
         const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
         handleAddition(req, res);
@@ -46,4 +47,56 @@ describe("POST /api/addition (mock req/res)", () => {
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({ resultat: -8 });
     });
+
+
+    test("5 + 3 = 8 avec des chaines", () => {
+        const req = { body: { nombre1: "5", nombre2: "3" } };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+        handleAddition(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ resultat: 8 });
+    });
+
+    test("Erreur 400 si un nombre manquant", () => {
+        const req = { body: { nombre1: 5} };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+        handleAddition(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ erreur: 'Veuillez fournir deux nombres' });
+    });
+
+    test("Erreur 400 si aucun nombre n'est renseigné", () => {
+        const req = { body: {} };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+        handleAddition(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ erreur: 'Veuillez fournir deux nombres' });
+    });
+
+    test("Erreur 400 si valeur non numérique", () => {
+        const req = { body: { nombre1: "test", nombre2: "oui"} };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+        handleAddition(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ erreur: 'Les paramètres doivent être des nombres valides' });
+    });
+
+    test("Erreur 400 si une ou plusieurs valeurs nulles", () => {
+        const req = { body: { nombre1: NaN, nombre2: NaN} };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+        handleAddition(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ erreur: 'Les paramètres doivent être des nombres valides' });
+    });
+
 });
